ce25c227679eb858f71ca9592e45d2931485e70d
kstrandholm <strandholm.kelly@gmail.com>
2020-06-16 16:29:55 -0500

    Adding PubSubEvent for show selected

---
+++ TVShowEvents.cs
@@ -0,0 +1,10 @@
+﻿using Prism.Events;
+
+namespace NET_TVShowPlaylist
+{
+	/// <inheritdoc/>
+	/// <summary>
+	/// Event that indicates TV Show has been selected from the show list on the Main Window.
+	/// </summary>
+	public class TVShowSelectedEvent : PubSubEvent { }
+}
