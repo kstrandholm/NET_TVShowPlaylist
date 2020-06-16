69f0330331953bed1200ac9b50d6b2463db381ea
kstrandholm <strandholm.kelly@gmail.com>
2020-06-16 14:12:53 -0500

    Adding new window for TVShowDetail.
    Included under a new folder.

---
+++ TVShowDetail/TVShowDetailWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace NET_TVShowPlaylist.TVShowDetail
+{
+	/// <summary>
+	/// Interaction logic for TVShowDetailWindow.xaml
+	/// </summary>
+	public partial class TVShowDetailWindow : Window
+	{
+		public TVShowDetailWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
