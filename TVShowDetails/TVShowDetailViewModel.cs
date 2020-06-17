262daa9c7eb39e781b0d0447e0c37768f0a79955
kstrandholm <strandholm.kelly@gmail.com>
2020-06-17 13:10:43 -0500

    Pop up dialog improvements.
    It works now!

--- TVShowDetails/TVShowDetailViewModel.cs
+++ TVShowDetails/TVShowDetailViewModel.cs
@@ -1,70 +1,76 @@
-﻿using NET_TVShowPlaylist.Models;
-using Prism.Commands;
+﻿using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
-using Prism.Regions;
 using Prism.Services.Dialogs;
 using System;
 
 namespace NET_TVShowPlaylist.TVShowDetails
 {
 	public class TVShowDetailViewModel : BindableBase, IDialogAware
-	{
-		private readonly IEventAggregator _eventAggregator;
-		private readonly IRegionManager _regionManager;
+    {
+        private readonly IEventAggregator _eventAggregator;
 
-		private DelegateCommand<string> _closeDialogCommand;
-		public DelegateCommand<string> CloseDialogCommand =>
-			_closeDialogCommand ?? (_closeDialogCommand = new DelegateCommand<string>(CloseDialog));
+        private DelegateCommand<string> _closeDialogCommand;
+        public DelegateCommand<string> CloseDialogCommand =>
+            _closeDialogCommand ?? (_closeDialogCommand = new DelegateCommand<string>(CloseDialog));
 
-		public string Title => throw new NotImplementedException();
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
 
-		public event Action<IDialogResult> RequestClose;
+        private string _title = "Notification";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
 
-		public bool CanCloseDialog() => true;
+        public event Action<IDialogResult> RequestClose;
 
-		/// <summary>
-		/// Constructor for a new TV Show Detail Window.
-		/// </summary>
-		public TVShowDetailViewModel(IEventAggregator eventAggregator, IRegionManager regionManager)
-		{
-			_regionManager = regionManager;
-			_eventAggregator = eventAggregator;
+        /// <summary>
+        /// Constructor for a new TV Show Detail Window.
+        /// </summary>
+        public TVShowDetailViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
 
-			// Subscribe to Events
-			_eventAggregator.GetEvent<TVShowSelectedEvent>().Subscribe(TVShowSelected);
-		}
+            // Subscribe to Events
+            //_eventAggregator.GetEvent<TVShowSelectedEvent>().Subscribe(TVShowSelected);
+        }
 
-		private void CloseDialog(string parameter)
-		{
-			ButtonResult result = ButtonResult.None;
+        protected virtual void CloseDialog(string parameter)
+        {
+            ButtonResult result = ButtonResult.None;
 
-			if (parameter?.ToLower() == "true")
-				result = ButtonResult.OK;
-			else if (parameter?.ToLower() == "false")
-				result = ButtonResult.Cancel;
+            if (parameter?.ToLower() == "true")
+                result = ButtonResult.OK;
+            else if (parameter?.ToLower() == "false")
+                result = ButtonResult.Cancel;
 
-			RaiseRequestClose(new DialogResult(result));
-		}
+            RaiseRequestClose(new DialogResult(result));
+        }
 
-		public virtual void RaiseRequestClose(IDialogResult dialogResult)
-		{
-			RequestClose?.Invoke(dialogResult);
-		}
+        public virtual void RaiseRequestClose(IDialogResult dialogResult)
+        {
+            RequestClose?.Invoke(dialogResult);
+        }
 
-		public void OnDialogClosed()
-		{
-			throw new NotImplementedException();
-		}
+        public virtual bool CanCloseDialog()
+        {
+            return true;
+        }
 
-		public void OnDialogOpened(IDialogParameters parameters)
-		{
-			throw new NotImplementedException();
-		}
+        public virtual void OnDialogClosed()
+        {
 
-		private void TVShowSelected(TVShow selectedShow)
-		{
+        }
 
-		}
+        public virtual void OnDialogOpened(IDialogParameters parameters)
+        {
+            Message = parameters.GetValue<string>("message");
+        }
 	}
 }
